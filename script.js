33ccbd266410623a3195b7fe9d773ba413b35a19
Gabriele <gabrimassi@rocketmail.com>
2022-06-01 17:44:41 +0200

    Added working game logic

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
-computerPlay(){ //Returns rock, paper or scissors; Rand
-
+function computerPlay(){ //Returns rock, paper or scissors; Rand
+    let computerSelection, gener;
+    gener=Math.floor(Math.random()*3);
+    switch(gener){
+        case 0:
+            computerSelection="rock";
+            break;
+        case 1:
+            computerSelection="paper";
+            break;
+        case 2:
+            computerSelection="scissors"
+            break;
+    }
+    return computerSelection;
 }
 
-playGame(playerSelection/*non casesensitive*/ */, computerSelection){ //Returns (ex.)"You lose! X beats Y"
-
+function playGame(playerSelection/*non casesensitive*/, computerSelection){ //Returns (ex.)"You lose! X beats Y"
+    //let draw, playerWin, playerLose;
+    switch(playerSelection){
+        case "rock":
+            if(computerSelection==="rock") return "That's a draw!";
+            if(computerSelection==="paper") return "You lose! "+computerSelection+" beats "+playerSelection;
+            if(computerSelection==="scissors") return "You win! "+playerSelection+" beats "+computerSelection;
+        case "paper":
+            if(computerSelection==="paper") return "That's a draw!";
+            if(computerSelection==="scissors") return "You lose! "+computerSelection+" beats "+playerSelection;
+            if(computerSelection==="rock") return "You win! "+playerSelection+" beats "+computerSelection;
+        case "scissors":
+            if(computerSelection==="scissors") return "That's a draw!";
+            if(computerSelection==="rock") return "You lose! "+computerSelection+" beats "+playerSelection;
+            if(computerSelection==="paper") return "You win! "+playerSelection+" beats "+computerSelection;
+    }
 }
+
+let playerSelection="paper";
+console.log("player selection: "+playerSelection);
+console.log(playGame(computerPlay(), playerSelection));
